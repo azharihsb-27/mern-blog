cf6ec34edae2eed8ad2025ca2fa211a78fd0a41c
azhari hsb <azhari.hsb27@gmail.com>
2024-08-01 20:30:02 +0700

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -28,3 +28,54 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getposts = async (req, res, next) => {
+  try {
+    // start of the posts we want to get
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    // the limit of the posts we want to get
+    const limit = parseInt(req.query.limit) || 9;
+    // the order of the posts we want to get
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+
+    const posts = await Post.find({
+      // searching post based on userId
+      ...(req.query.userId && { userId: req.query.userId }),
+      // searching post based on category
+      ...(req.query.category && { category: req.query.category }),
+      // searching post based on slug
+      ...(req.query.slug && { slug: req.query.slug }),
+      // searching post based on postId
+      ...(req.query.postId && { _id: req.query.postId }),
+      // searching post based on title & content
+      ...(req.query.searchTerms && {
+        // $or allow us to search between multiple conditions
+        $or: [
+          // $options: 'i' means case insensitive
+          { title: { $regex: req.query.searchTerms, $options: 'i' } },
+          { content: { $regex: req.query.searchTerms, $options: 'i' } },
+        ],
+      }),
+      // sorting the posts based on updatedAt
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    // get last month posts
+    const lastMonthPosts = await Post.countDocuments({
+      updatedAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({ posts, totalPosts, lastMonthPosts });
+  } catch (error) {
+    next(error);
+  }
+};
